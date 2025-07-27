51780c9797798ed1a3c71cdf9515fcb708a2b9d4
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-07-27 14:04:55 +0330

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+const webPort = "8080"
+
+func main(){
+	//connect db
+
+	// create sessions
+
+	//create channels
+
+	//create wait groups
+
+	//set up application config
+
+	// set up email
+
+	//listen for web connection
+}
