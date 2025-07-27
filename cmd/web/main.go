1e251a2610872640e3c2d9ebfa584dec331c24bd
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-07-27 17:49:41 +0330

    redis connection and config app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"os"
+	"sync"
 	"time"
 
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
@@ -16,15 +21,24 @@ const webPort = "8080"
 func main() {
 	//connect db
 	db := initDB()
-	db.Ping()
+
 	// create sessions
+	session := initSession()
+
+	//create loggers
 
 	//create channels
 
 	//create wait groups
-
+	wg := sync.WaitGroup{}
 	//set up application config
-
+	app := Config{
+		Session: session,
+		DB:      db,
+		InfoLog: log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.Lshortfile),
+		ErrorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		Wait:    &wg,
+	}
 	// set up email
 
 	//listen for web connection
@@ -75,4 +89,26 @@ func openDB(dsn string) (*sql.DB,error){
 		return nil , err
 	}
 	return db, nil
+}
+
+func initSession() *scs.SessionManager{
+	session := scs.New()
+	// Initialize Redis for session storage
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+
+	return session
+}
+
+func initRedis() *redis.Pool{
+	redisPool := &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return redisPool
 }
