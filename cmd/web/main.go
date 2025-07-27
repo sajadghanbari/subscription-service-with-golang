cdfbfbe0fc6e179d5cea6b23074c4b864bba8e0b
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-07-27 19:20:16 +0330

    sgraceful shuting down

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/alexedwards/scs/redisstore"
@@ -43,6 +45,8 @@ func main() {
 	}
 	// set up email
 
+	//listen for signals
+	go app.listenFotShutdown()
 	//listen for web connection
 	app.serve()
 }
@@ -128,4 +132,19 @@ func initRedis() *redis.Pool{
 		},
 	}
 	return redisPool
+}
+
+func (app *Config) listenFotShutdown(){
+	quit := make(chan os.Signal,1)
+	signal.Notify(quit, syscall.SIGINT,syscall.SIGTERM)
+	<-quit
+	app.shutdown()
+	os.Exit(0)
+}
+
+func (app *Config) shutdown(){
+	app.InfoLog.Println("would run cleanup tasks...")
+	// block until wait group 
+	app.Wait.Wait()
+	app.InfoLog.Println("closing channels and shuting down.....")
 }
