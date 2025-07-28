efac764d5f192b29b7f9deb6ca60aa1815252924
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-07-28 14:07:58 +0330

    Starting on the handler to create a user

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"subscription-service/data"
+)
 
 func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {
 	app.render(w,r, "home.page.gohtml", nil)
@@ -43,7 +48,7 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 		}
 
 		app.sendEmail(msg)
-		
+
 		app.Session.Put(r.Context(),"error","Invalid credentials")
 		http.Redirect(w,r,"/login",http.StatusSeeOther)
 		return 
@@ -72,9 +77,46 @@ func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
 	app.render(w,r, "register.page.gohtml", nil)
 }
 func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
-	// Handle registration logic here
-	// For now, just redirect to home page
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	err := r.ParseForm()
+	if err != nil{
+		app.ErrorLog.Println(err)
+	}
+	//validate data
+
+	//create a user
+	u := data.User{
+		Email: r.Form.Get("email"),
+		FirstName: r.Form.Get("first-name"),
+		LastName: r.Form.Get("last-name"),
+		Password: r.Form.Get("password"),
+		Active: 0,
+		IsAdmin: 0,
+	}
+
+	_, err = u.Insert(u)
+	if err != nil {
+		app.Session.Put(r.Context(),"error","Unable to create user.")
+		http.Redirect(w,r,"/register",http.StatusSeeOther)
+		return
+	}
+
+	url := fmt.Sprintf("http://localhost:8080/activate?email=%s",u.Email)
+	signedURL := GenerateTokenFromString(url)
+	app.InfoLog.Println(signedURL)
+
+	msg := Message{
+		To: u.Email,
+		Subject: "Activate account",
+		Template: "confirm-email",
+		Data: template.HTML(signedURL),
+	}
+
+	app.sendEmail(msg)
+	app.Session.Put(r.Context(),"flash","Confirmation Email sent")
+	http.Redirect(w,r,"/login",http.StatusSeeOther)
+
+	
+	
 }
 
 func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
